d3d14285ac8236e0f9074e59b940ab454950dced
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-07-17 12:23:28 -0300

    Criado o end point para mecher no position das listas.

--- src/main/java/com/drufontael/dslist/services/GameListService.java
+++ src/main/java/com/drufontael/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.drufontael.dslist.services;
 
 import com.drufontael.dslist.dto.GameListDTO;
 import com.drufontael.dslist.entities.GameList;
+import com.drufontael.dslist.projections.GameMinProjection;
 import com.drufontael.dslist.repositories.GameListRepository;
+import com.drufontael.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +15,25 @@ import java.util.List;
 public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
+    @Autowired
+    private GameRepository gameRepository;
 
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result=gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+    @Transactional(readOnly = true)
+    public void move(Long listId,int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list=gameRepository.searchByList(listId);
+        GameMinProjection obj=list.remove(sourceIndex);
+        list.add(destinationIndex,obj);
+        int min=sourceIndex<destinationIndex?sourceIndex:destinationIndex;
+        int max=sourceIndex>destinationIndex?sourceIndex:destinationIndex;
+        for (int i=min;i<=max;i++){
+            gameListRepository.updateBelongingPosition(listId,list.get(i).getId(),i);
+        }
+
     }
 }
